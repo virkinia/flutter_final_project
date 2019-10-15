73091cc0bad13ce059a42ffd572215d38b6613ba
Virllinia <virlli_nia@hotmail.com>
2019-10-15 21:45:22 +0200

    show first input login

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -14,6 +14,7 @@ class _LoginScreenState extends State<LoginScreen> {
       children: <Widget>[
         Container(
           width: double.infinity,
+          padding: EdgeInsets.symmetric(vertical: 60),
           decoration: BoxDecoration(
             gradient:
                 LinearGradient(colors: [Colors.cyan[300], Colors.cyan[800]]),
@@ -21,9 +22,25 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Image.asset(
             "assets/logo.png",
             color: Colors.white,
+            height: 200,
           ),
-          height: 200,
         ),
+        Center(
+          child: Card(
+            margin: const EdgeInsets.only(left: 20, right: 20, top: 260),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(labelText: "Usuario:"),
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
       ],
     ));
   }
